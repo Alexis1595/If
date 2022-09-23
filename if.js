9b2d7e831a4462bcd8732e91935b8a17af41ce17
Alexis1595 <113454850+Alexis1595@users.noreply.github.com>
2022-09-23 11:35:05 +0200

    Create if.js

---
+++ if.js
@@ -0,0 +1,15 @@
+package com.bootcamp;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        boolean check = 5 < 10;
+
+        int number1 = 45;
+        int number2 = 49;
+        int number3 = 50;
+        if(number1 < number2 && number2 < number3){
+            System.out.println("Verdadero");
+        }
+    }
+}
